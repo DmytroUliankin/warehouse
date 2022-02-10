38490220e0044de323f578efccf375cad22a5e72
Dmitry Uliankin <dimmich2001@gmail.com>
2022-02-10 16:29:18 +0200

    updated "SELECT_USER_BY_ID" query

--- src/main/java/com/warehouse/util/UserQueryUtil.java
+++ src/main/java/com/warehouse/util/UserQueryUtil.java
@@ -14,6 +14,6 @@ public class UserQueryUtil {
             "place_of_birth, current_position, address_of_living  FROM users";
 
     public static final String SELECT_USER_BY_ID = "SELECT user_id, user_role, user_name, surname, email, year_of_birth, " +
-            "place_of_birth, current_position, address_of_living  FROM users WHERE user_id";
+            "place_of_birth, current_position, address_of_living  FROM users WHERE user_id = ?";
 
 }
